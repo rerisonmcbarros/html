f262feba0482022b7431dab8308ef06bbe42622b
Rerison Barros <rerisonmcb@gmail.com>
2023-03-10 15:50:56 -0300

    Criação da classe 'ProductRepository'

---
+++ App/Model/Repository/ProductRepository.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Model\Repository;
+
+use App\Model\Product;
+use Lib\Database\Mapper;
+use Lib\Database\Transaction;
+
+class ProductRepository extends Mapper
+{
+	public function __construct()
+	{
+		parent::__construct( new Product() );
+	}
+
+	public function findByCodigo( string $codigo, string $columns = '*' )
+	{
+		try
+		{
+			$conn = Transaction::get();
+
+			$query = "SELECT {$columns} FROM ".$this->getEntity()." WHERE codigo = :codigo";
+			$stmt = $conn->prepare( $query );
+			$stmt->bindValue( ":codigo", $codigo, \PDO::PARAM_STR );
+			$stmt->execute();
+
+			Transaction::log( $this->getQueryLog( $query, ['codigo'=>$codigo] ) );
+
+			return $stmt->fetchObject( get_class( $this->model ) );
+		}
+		catch( \PDOException $e )
+		{
+			return $e->getMessage();
+		}
+	}
+
+	public function findByCategoria( string $nome, $limit = null, $offset = 0 )
+	{
+		try
+		{
+			if( $limit == null )
+			{
+				$limit = $this->findByCategoriaCount( $nome );
+			}
+			$conn = Transaction::get();
+
+			$query = "SELECT 
+			produto.id,
+			produto.codigo,
+			produto.descricao,
+			produto.preco_custo,
+			produto.preco_venda,
+			produto.estoque,
+			categoria.codigo as codigo_categoria,
+			categoria.nome as nome_categoria  
+			FROM ".$this->getEntity()." INNER JOIN categoria 
+			ON produto.id_categoria = categoria.id WHERE categoria.nome 
+			LIKE CONCAT('%',:nome, '%') LIMIT :limit OFFSET :offset";
+
+			$stmt = $conn->prepare( $query );
+
+			$stmt->bindValue( ":nome", $nome, \PDO::PARAM_STR );
+			$stmt->bindValue( ":limit", $limit, \PDO::PARAM_INT );
+			$stmt->bindValue( ":offset", $offset, \PDO::PARAM_INT );
+
+			$stmt->execute();
+
+			Transaction::log( $this->getQueryLog( $query, ['nome'=> $nome] ) );
+
+			return $stmt->fetchAll( \PDO::FETCH_CLASS , get_class( $this->model ) );
+		}
+		catch( \PDOException $e )
+		{
+			return $e->getMessage();
+		}
+	}	
+
+	public function findByCategoriaCount( string $nome ) 
+	{
+		try
+		{
+			$conn = Transaction::get();
+
+			$query = "SELECT count(categoria.nome) as count FROM ".$this->getEntity()." INNER JOIN categoria 
+			ON produto.id_categoria = categoria.id WHERE categoria.nome LIKE CONCAT('%',:nome, '%') ";
+			
+			$stmt = $conn->prepare( $query );
+			$stmt->bindValue( ":nome", $nome, \PDO::PARAM_STR );
+			$stmt->execute();
+
+			Transaction::log( $this->getQueryLog( $query, ['nome'=> $nome] ) );
+
+			return $stmt->fetch( \PDO::FETCH_ASSOC )['count'] ?? null;
+		}
+		catch( \PDOException $e )
+		{
+			return $e->getMessage();
+		}
+	}
+
+	public function getProdutoCategoria( $limit = null, $offset = 0 )
+	{
+		try
+		{
+			if($limit == null)
+			{
+				$limit = $this->getLastId();
+			}
+			$conn = Transaction::get();
+
+			$query = "SELECT 
+			produto.id,
+			produto.codigo,
+			produto.descricao,
+			produto.preco_custo,
+			produto.preco_venda,
+			produto.estoque,
+			categoria.nome as nome_categoria  
+			FROM ".$this->getEntity()." INNER JOIN categoria 
+			ON produto.id_categoria = categoria.id ORDER BY produto.id LIMIT :limit OFFSET :offset";
+
+			$stmt = $conn->prepare( $query );
+			$stmt->bindValue( ":limit", $limit, \PDO::PARAM_INT );
+			$stmt->bindValue( ":offset", $offset, \PDO::PARAM_INT );
+			$stmt->execute();
+
+			Transaction::log( $query );
+
+			return $stmt->fetchAll( \PDO::FETCH_CLASS , get_class( $this->model ) );
+		}
+		catch( \PDOException $e )
+		{
+			return $e->getMessage();
+		}
+	}
+}
