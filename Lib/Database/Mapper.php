6dde10b9cf2fcd7057e9c3951963f3d677808701
Rerison Barros <rerisonmcb@gmail.com>
2023-03-10 18:54:51 -0300

    Removendo funcionalidades desnecessárias da classe 'Mapper'

--- Lib/Database/Mapper.php
+++ Lib/Database/Mapper.php
@@ -25,136 +25,101 @@ class Mapper
 
 	public function find( int $id, string $columns = '*' )
     {
-		try
-        {
-			$conn = Transaction::get();
+		$conn = Transaction::get();
 
-			$query = "SELECT {$columns} FROM {$this->getEntity()} WHERE id = :id";
-			$stmt = $conn->prepare( $query );
-			$stmt->bindValue( ":id", $id, \PDO::PARAM_INT );
-			$stmt->execute();
+		$query = "SELECT {$columns} FROM {$this->getEntity()} WHERE id = :id";
+		$stmt = $conn->prepare( $query );
+		$stmt->bindValue( ":id", $id, \PDO::PARAM_INT );
+		$stmt->execute();
 
-			Transaction::log( $this->getQueryLog( $query,['id'=> $id] ) );
+		Transaction::log( $this->getQueryLog( $query,['id'=> $id] ) );
 
-			return $stmt->fetchObject( get_class( $this->model ) );
-		}
-		catch( \PDOException $e )
-        {
-			return $e->getMessage();
-		}
+		return $stmt->fetchObject( get_class( $this->model ) );	
 	}
 
 	public function all( $limit = null, $offset = 0, string $columns = '*' )
 	{
-		try
+		if($limit == null)
 		{
-			if($limit == null)
-			{
-				$limit = $this->getLastId();
-			}
-			$conn = Transaction::get();
-
-			$query = "SELECT {$columns} FROM {$this->getEntity()} LIMIT :limit OFFSET :offset";
-
-			$stmt = $conn->prepare( $query );
-
-			$stmt->bindValue( ":limit", $limit, \PDO::PARAM_INT );
-			$stmt->bindValue( ":offset", $offset, \PDO::PARAM_INT );
-            $stmt->execute();
-
-			Transaction::log( $this->getQueryLog( $query,['limit' => (int)$limit, 'offset' => (int)$offset] ) );
-
-			return $stmt->fetchAll( \PDO::FETCH_CLASS, get_class( $this->model ) );
+			$limit = $this->getLastId();
 		}
-		catch( \PDOException $e )
-		{
-			return $e->getMessage();
-		}
+		$conn = Transaction::get();
+
+		$query = "SELECT {$columns} FROM {$this->getEntity()} LIMIT :limit OFFSET :offset";
+
+		$stmt = $conn->prepare( $query );
+
+		$stmt->bindValue( ":limit", $limit, \PDO::PARAM_INT );
+		$stmt->bindValue( ":offset", $offset, \PDO::PARAM_INT );
+		$stmt->execute();
+
+		Transaction::log( $this->getQueryLog( $query,['limit' => (int)$limit, 'offset' => (int)$offset] ) );
+
+		return $stmt->fetchAll( \PDO::FETCH_CLASS, get_class( $this->model ) );
 	}
 
 	public function count()
 	{
-		try
-		{
-			$conn = Transaction::get();
+		$conn = Transaction::get();
 
-			$query = "SELECT count(id) as count FROM {$this->getEntity()}";
-			$stmt = $conn->prepare( $query );
-			$stmt->execute();
+		$query = "SELECT count(id) as count FROM {$this->getEntity()}";
+		$stmt = $conn->prepare( $query );
+		$stmt->execute();
 
-			Transaction::log( $query );
+		Transaction::log( $query );
 
-			return $stmt->fetch( \PDO::FETCH_ASSOC )['count'];
-		}
-		catch( \PDOException $e )
-		{
-			return $e->getMessage();
-		}
+		return $stmt->fetch( \PDO::FETCH_ASSOC )['count'];
 	}
 
 	public function getLastId()
 	{
-		try
-		{
-			$conn = Transaction::get();
+		$conn = Transaction::get();
 
-			$query = "SELECT max(id) as last FROM {$this->getEntity()}";
-			$stmt = $conn->prepare( $query );
-			$stmt->execute();
+		$query = "SELECT max(id) as last FROM {$this->getEntity()}";
+		$stmt = $conn->prepare( $query );
+		$stmt->execute();
 
-			Transaction::log( $query );
+		Transaction::log( $query );
 
-			return $stmt->fetch( \PDO::FETCH_ASSOC )['last'] ?? 0;
-		}
-		catch( \PDOException $e )
-		{
-			return $e->getMessage();
-		}
+		return $stmt->fetch( \PDO::FETCH_ASSOC )['last'] ?? 0;
 	}
 
 	public function store(ModelInterface $model)
 	{
-		try
-		{
-			$this->model = $model;
+		$this->model = $model;
 
-			if( empty( $this->model->id ) )
+		if( empty( $this->model->id ) )
+		{
+			$this->model->id = $this->getLastId() + 1;
+			$columns = implode( ", ", array_keys( $this->getData() ) );
+			$values = ":".implode( ", :", array_keys( $this->getData() ) );
+			$query = "INSERT INTO {$this->getEntity()} ({$columns}) VALUES ({$values})";
+		}
+		else
+		{
+			$set = [];
+			foreach( array_keys( $this->getData() ) as $key => $value )
 			{
-				$this->model->id = $this->getLastId() + 1;
-				$columns = implode( ", ", array_keys( $this->getData() ) );
-				$values = ":".implode( ", :", array_keys( $this->getData() ) );
-				$query = "INSERT INTO {$this->getEntity()} ({$columns}) VALUES ({$values})";
-			}
-			else
-			{
-				$set = [];
-				foreach( array_keys( $this->getData() ) as $key => $value )
+				if( $value != 'id' )
 				{
-					if( $value != 'id' )
-					{
-						$set[$key] = $value." = :".$value;
-					}	
-				}
-				$set = implode( ", ", array_values( $set ) );
-				$query = "UPDATE {$this->getEntity()} SET {$set} WHERE id = :id";	
+					$set[$key] = $value." = :".$value;
+				}	
 			}
-			$conn = Transaction::get();
-			$stmt = $conn->prepare( $query );
-
-			foreach( $this->getData()  as $key => $value )
-			{
-				$type = is_int( $value ) ? \PDO::PARAM_INT : \PDO::PARAM_STR;  	
-				$stmt->bindValue( ":{$key}", $value, $type );
-			}
-
-			Transaction::log( $this->getQueryLog( $query, $this->getData() ) );
-
-			return $stmt->execute();
+			$set = implode( ", ", array_values( $set ) );
+			$query = "UPDATE {$this->getEntity()} SET {$set} WHERE id = :id";	
 		}
-		catch( \PDOException $e )
+		$conn = Transaction::get();
+		$stmt = $conn->prepare( $query );
+
+		foreach( $this->getData()  as $key => $value )
 		{
-			return $e->getMessage();
+			$type = is_int( $value ) ? \PDO::PARAM_INT : \PDO::PARAM_STR;  	
+			$stmt->bindValue( ":{$key}", $value, $type );
 		}
+
+		Transaction::log( $this->getQueryLog( $query, $this->getData() ) );
+
+		return $stmt->execute();
 	}
 
 	public function delete( $id = null )
@@ -163,22 +128,16 @@ class Mapper
         {
             $id = $this->model->id;
         }
-		try
-		{
-			$conn = Transaction::get();
+	
+		$conn = Transaction::get();
 
-			$query = "DELETE FROM {$this->getEntity()} WHERE id = :id";
-			$stmt = $conn->prepare( $query );
-			$stmt->bindValue( ":id", $id, \PDO::PARAM_INT );		
-			
-			Transaction::log( $this->getQueryLog( $query, ['id'=>$id] ) );
+		$query = "DELETE FROM {$this->getEntity()} WHERE id = :id";
+		$stmt = $conn->prepare( $query );
+		$stmt->bindValue( ":id", $id, \PDO::PARAM_INT );		
+		
+		Transaction::log( $this->getQueryLog( $query, ['id'=>$id] ) );
 
-			return $stmt->execute();
-		}
-		catch( \PDOException $e )
-		{
-			return $e->getMessage();
-		}
+		return $stmt->execute();
 	}
 
 	protected function getQueryLog( string $query, array $data )
