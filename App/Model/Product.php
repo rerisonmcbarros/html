6596b1531cea26cf4c94c5b7a7f099ee01c392ea
Rerison Barros <rerisonmcb@gmail.com>
2023-03-16 14:32:42 -0300

    Alteração na classe 'Product'

--- App/Model/Product.php
+++ App/Model/Product.php
@@ -6,17 +6,15 @@ use Lib\Database\Record;
 use Lib\Database\Transaction;
 
 class Product extends Model
-{	
-	public static $entity = 'produto';
-
+{		
 	public function setCodigo( $codigo )
 	{
-		if( $codigo === null || $codigo === '' )
+		if( $codigo === null || $codigo === '' ) 
 		{
 			throw new \Exception("O campo código não pode estar vazio!");
 		}
 		
-		if( !is_numeric( $codigo ) )
+		if( !is_numeric( $codigo ) ) 
 		{
 			throw new \Exception("O campo código deve ser um valor numérico!");
 		}
@@ -26,13 +24,11 @@ class Product extends Model
 
 	public function setIdCategoria( $id_categoria )
 	{
-		if( $id_categoria === null || $id_categoria === '' )
-		{
+		if( $id_categoria === null || $id_categoria === '' ) {
 			throw new \Exception("O nome da categoria não pode estar vazio!");
 		}
 
-		if( !is_numeric( $id_categoria ) )
-		{
+		if( !is_numeric( $id_categoria ) ) {
 			throw new \Exception("O campo categoria deve ser um valor numérico!");
 		}
 
@@ -41,13 +37,11 @@ class Product extends Model
 
 	public function setDescricao( $descricao )
 	{
-		if( $descricao === null || $descricao === '' )
-		{
+		if( $descricao === null || $descricao === '' ) {
 			throw new \Exception("A descrição não pode estar vazia!");
 		}
 
-		if( is_numeric( $descricao ) )
-		{
+		if( is_numeric( $descricao ) ) {
 			throw new \Exception("O campo descrição não pode ser um valor numérico!");
 		}
 
@@ -58,13 +52,11 @@ class Product extends Model
 	{
 		$preco_custo = str_replace( ",", ".", $preco_custo );
 
-		if( $preco_custo === null || $preco_custo === '' )
-		{
+		if( $preco_custo === null || $preco_custo === '' ) {
 			throw new \Exception("O campo de preço de custo não pode estar vazio!");
 		}
 
-		if( !is_numeric( $preco_custo ) )
-		{
+		if( !is_numeric( $preco_custo ) ) {
 			throw new \Exception("O campo de preço de custo deve ser um valor numérico!");
 		}
 
@@ -75,13 +67,11 @@ class Product extends Model
 	{
 		$preco_venda = str_replace( ",", ".", $preco_venda );
 
-		if( $preco_venda === null || $preco_venda === '' )
-		{
+		if( $preco_venda === null || $preco_venda === '' ) {
 			throw new \Exception("O campo de preço de venda não pode estar vazio!");
 		}
 
-		if( !is_numeric( $preco_venda ) )
-		{
+		if( !is_numeric( $preco_venda ) ) {
 			throw new \Exception("O campo de preço de venda deve ser um valor numérico!");
 		}
 
@@ -90,17 +80,14 @@ class Product extends Model
 
 	public function setEstoque( $estoque )
 	{
-		if( $estoque === null || $estoque === '' )
-		{
+		if( $estoque === null || $estoque === '' ) {
 			throw new \Exception("O campo estoque não pode estar vazio!");		
 		}
 
-		if( !is_numeric( $estoque ) && $estoque >= 0 )
-		{
+		if( !is_numeric( $estoque ) && $estoque >= 0 ) {
 			throw new \Exception("O campo estoque deve ser um valor numérico inteiro!");
 		}
 
 		$this->data['estoque'] = $estoque;
 	}
 }
-
