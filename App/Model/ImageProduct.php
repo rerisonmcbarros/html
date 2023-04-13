9dbdb7f00a64f2252227a0f611793550c3abf62c
Rerison Barros <rerisonmcb@gmail.com>
2023-04-13 11:35:08 -0300

    Alteração na classe 'ImageProduct'

--- App/Model/ImageProduct.php
+++ App/Model/ImageProduct.php
@@ -2,37 +2,9 @@
 
 namespace App\Model;
 
-use Lib\Database\Record;
-use Lib\Database\Transaction;
+//Funcionalidade para ser posteriormente implementada
+//Classe de modelo para imagem de produtos
+class ImageProduct extends Model{
 
-class ImageProduct extends Record{
-
-
-	public const TABLE_NAME = "imagem_produto";
-
-	public function findByProduto($id_produto, $columns = '*'){
-
-		try{
-
-			$conn = Transaction::get();
-
-			$query = "SELECT {$columns} FROM ".self::TABLE_NAME." WHERE id_produto = :id_produto";
-
-			$stmt = $conn->prepare($query);
-
-			$stmt->bindValue(":id_produto", $id_produto, \PDO::PARAM_INT);
-
-			$stmt->execute();
-
-			Transaction::log($this->getQueryLog($query, ['id_produto'=>$id_produto]));
-
-			return $stmt->fetchAll(\PDO::FETCH_CLASS,get_class($this));
-		}
-		catch(\PDOException $e){
-
-			return $e->getMessage();
-		}
-
-	}
 
 }
